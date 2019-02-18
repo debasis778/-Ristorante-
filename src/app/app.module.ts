c5eef9d70c5c9912cd84b8299537be72011e3def
debasis778 <debasis778@gmail.com>
2019-02-18 17:54:18 +0530

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
